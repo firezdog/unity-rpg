0032bdd82b9baed7c2ffbff6de414697d594eec3
Alex Leibowitz <alexanderbleibowitz@gmail.com>
2018-11-18 22:38:21 -0600

    added player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using keyCode = UnityEngine.KeyCode;
+
+public class PlayerController : MonoBehaviour {
+
+	public Rigidbody2D playerBody;
+	// Use this for initialization
+	void Start () {}
+	
+	// Update is called once per frame
+	void Update () {
+		playerBody.velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+	}
+
+}
