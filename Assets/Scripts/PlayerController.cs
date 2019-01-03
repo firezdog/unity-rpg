b7794efe0812cff60f125ddcfadbbff79a0e24d1
Alex Leibowitz <alexanderbleibowitz@gmail.com>
2019-01-03 23:35:56 +0800

    plug in animations to movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,8 +6,9 @@ using keyCode = UnityEngine.KeyCode;
 
 public class PlayerController : MonoBehaviour {
 
-	public Rigidbody2D playerBody;
+	public Animator playerAnimator;
 	public float moveSpeed;
+	public Rigidbody2D playerBody;
 
 	// Use this for initialization
 	void Start () {
@@ -15,7 +16,25 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		playerAnimator.SetBool("stopped", false);
 		playerBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
+		playerAnimator.SetFloat("moveX", playerBody.velocity.x);
+		playerAnimator.SetFloat("moveY", playerBody.velocity.y);
+		if (playerBody.velocity.x > 0) {
+			playerAnimator.SetFloat("facingX", 1.0f);
+			playerAnimator.SetFloat("facingY", 0.0f);
+		} else if (playerBody.velocity.x < 0) {
+			playerAnimator.SetFloat("facingX", -1.0f);
+			playerAnimator.SetFloat("facingY", 0.0f);
+		} else if (playerBody.velocity.y > 0) {
+			playerAnimator.SetFloat("facingX", 0.0f);
+			playerAnimator.SetFloat("facingY", 1.0f);
+		} else if (playerBody.velocity.y < 0) {
+			playerAnimator.SetFloat("facingX", 0.0f);
+			playerAnimator.SetFloat("facingY", -1.0f);
+		} else {
+			playerAnimator.SetBool("stopped", true);
+		}
 	}
 
 }
