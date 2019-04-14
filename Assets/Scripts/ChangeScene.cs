1bd425924bd218c50cf8b2d10adeb2a0e41f5a95
Alex Leibowitz <firezdog@gmail.com>
2019-04-14 13:26:18 -0500

    added player loader

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -6,28 +6,38 @@ using UnityEngine.SceneManagement;
 public class ChangeScene : MonoBehaviour
 {
 
-    public string from;
-    public string to;
-    PlayerController p = PlayerController.instance;
-    static bool used = false;
+	public string from;
+	public string to;
+	PlayerController p = PlayerController.instance;
+	static bool used = false;
 
-    void Start() {
-        if (p == null) { p = GameObject.Find("Player").GetComponent<PlayerController>(); }
-        if (p.getFrom() == from) { p.transform.position = gameObject.transform.position; }
-    }
-    
-    private void OnTriggerEnter2D(Collider2D other) {
-        if (other.tag == "Player" && from != "start" && !used) {
-            SceneManager.LoadSceneAsync(to);
-            p.setFrom(from);
-            used = true;
-        }
-    }
+	void Start()
+	{
+		if (p == null)
+		{
+			p = GameObject
+				.FindGameObjectWithTag("Player")
+				.GetComponent<PlayerController>();
+		}
+		if (p.getFrom() == from) { p.transform.position = gameObject.transform.position; }
+	}
 
-    private void OnTriggerExit2D(Collider2D other) {
-        if (other.tag == "Player" && from != "start" && used) {
-            used = false;
-        }
-    }
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player" && from != "start" && !used)
+		{
+			SceneManager.LoadSceneAsync(to);
+			p.setFrom(from);
+			used = true;
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.tag == "Player" && from != "start" && used)
+		{
+			used = false;
+		}
+	}
 
 }
---
+++ Assets/Scripts/PlayerLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Plan: if the player is does not exist, instantiate. 
+// Doesn't this interfere with the behavior set out in Player script?
+
+public class PlayerLoader : MonoBehaviour
+{
+
+    // Get reference to player we want to spawn.
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (GameObject.FindGameObjectWithTag("Player") == null) {
+            Instantiate(player);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
