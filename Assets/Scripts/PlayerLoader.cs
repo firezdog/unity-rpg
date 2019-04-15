c7206a6800587576ae34f95cffd92e7f1efe267b
Alex Leibowitz <firezdog@gmail.com>
2019-04-14 21:54:43 -0500

    change awake to start method in playerLoader.cs

--- Assets/Scripts/PlayerLoader.cs
+++ Assets/Scripts/PlayerLoader.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 // Plan: if the player is does not exist, instantiate. 
 // Doesn't this interfere with the behavior set out in Player script?
@@ -8,20 +6,21 @@ using UnityEngine;
 public class PlayerLoader : MonoBehaviour
 {
 
-    // Get reference to player we want to spawn.
-    public GameObject player;
+	// Get reference to player we want to spawn.
+	public GameObject player;
 
-    // Start is called before the first frame update
-    void Awake()
-    {
-        if (GameObject.FindGameObjectWithTag("Player") == null) {
-            Instantiate(player);
-        }
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+		if (GameObject.FindGameObjectWithTag("Player") == null)
+		{
+			Instantiate(player);
+		}
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
 }
