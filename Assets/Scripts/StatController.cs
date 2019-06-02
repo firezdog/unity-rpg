4b81b21d70332aea404aec16a83f7d0b074c5298
Alex Leibowitz <alexanderbleibowitz@gmail.com>
2019-06-02 10:00:37 -0500

    remaking scenes; added stat controller

---
+++ Assets/Scripts/StatController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatController : MonoBehaviour
+{
+
+    private string playerName;
+    private Sprite playerImage;
+
+    private int currentHP;
+    private int maxHP;
+    private int currentMP;
+    private int maxMP;
+
+    private int attack;
+    private int weaponBonus;
+    private string equippedWeapon;
+    private int defence;
+    private int armorBonus;
+    private string equippedArmor;
+
+    private int exp;
+    private int level = 1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- stats_system.md
+++ stats_system.md
@@ -1,4 +1,8 @@
 # Section 7: Leveling Up
+Keep track of player stats on individual characters using stat manager script
+
+This should be really simple -- just a script with some variables (like health, magic, exp), getters, setters -- attached to player. Tricky point might be making sure that player doesn't get new stats each time she loads. Maybe instead of putting script on character, it is put on game manager (singleton) object?
+
 ## Player Stats
 ## Adding Leveling System
 ## Testing Leveling
