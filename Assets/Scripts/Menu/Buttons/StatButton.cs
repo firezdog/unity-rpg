80870fae5ded6407dd45845c53bbfac39a60b7c6
Alex Leibowitz <alex.leibowitz@zoro.com>
2019-09-16 19:35:15 -0500

    add new button prefab specific to stat menu; add onClick event to button script

---
+++ Assets/Scripts/Menu/Buttons/StatButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class StatButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick() {
+        print("hello");
+    }
+
+}
