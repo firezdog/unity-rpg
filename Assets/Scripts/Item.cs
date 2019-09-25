b0c3b42570a88aedf2e394af5994195ecde4c60c
Alex Leibowitz <alex.leibowitz@zoro.com>
2019-09-24 20:56:12 -0500

    added item wrapper gameobject

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -7,6 +7,9 @@ using UnityEngine.UI;
 public class Item : ScriptableObject
 {
     [SerializeField] string itemName;
+    public string ItemName { get => itemName; set => itemName = value; }
     [SerializeField, TextArea] string description;
+    public string Description { get => description; set => description = value; }
     [SerializeField] Sprite itemImage;
+    public Sprite ItemImage { get => itemImage; set => itemImage = value; }
 }
